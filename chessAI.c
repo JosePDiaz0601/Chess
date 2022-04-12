b523b3816002a7c84c872179b564f030f8b295c4
Paul Khayet <pkhayet@uci.edu>
2022-04-12 12:32:32 -0700

    Initial Change

--- chessAI.c
+++ chessAI.c
@@ -1,1 +1,15 @@
-hello world
+/*********************************************************************/
+/* Chess Assignment, for EECS 22L, Spring 22                         */
+/*                                                                   */
+/* Initial author: Paul Khayet                                       */
+/*                                                                   */
+/* ChessAI.c: C file for Artifical Intelligence                      */
+/*                                                                   */
+/*********************************************************************/
+
+#include <stdlib.h>
+#include "chessAI.h"
+
+void choosePiece(); 
+
+
