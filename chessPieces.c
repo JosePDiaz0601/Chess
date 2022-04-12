4936332b80364ef9f8a447e830e17e02948c74ee
Maher Homsi <homsim@uci.edu>
2022-04-12 12:56:41 -0700

    Beginning of chessPieces.c

--- chessPieces.c
+++ chessPieces.c
@@ -1,18 +1,84 @@
 #include "chessPieces.h"
 #include <stdlib.h>
+#include <stdbool.h>
 
-/*  1 - pawn 
-    2 - bishop
-    3 - knight
-    4 - rook
-    5 - queen
-    6 - king */
+/*  1 - wpawn 
+    2 - wbishop
+    3 - wknight
+    4 - wrook
+    5 - wqueen
+    6 - wking 
+    7 - bpawn
+    8 - bbishop
+    9 - bknight
+    10- brook
+    11- bqueen
+    12- bking*/
 int type, x, y, color;
 bool hasMoved;
 int rangeOfMotion[][];
 
-void getRangeOfMotion(void){
-    if piece == 3{
-        rangeOfMotion[][]
+void getRangeOfMotion(void)
+{
+    if piece == 1
+    {
+        if y < 7
+        {
+            rangeOfMotion[x][y+1] = 1;
+            if hasMoved != 1
+            {
+                rangeOfMotion[x][y+2] = 1;
+            }
+        }
+    }
+    
+    if piece == 3
+    {
+        if y+3 < 8 
+        {
+            if x+1 < 8
+            {
+                rangeOfMotion[x+1][y+3] = 1;
+            }
+            if x-1 >= 0
+            {
+                rangeOfMotion[x-1][y+3] = 1;
+            }
+        }
+        if y-3 >= 0
+        {
+            if x+1 < 8
+            {
+                rangeOfMotion[x+1][y-3] = 1;
+            }
+            if x-1 >= 0
+            {
+                rangeOfMotion[x-1][y-3] = 1;
+            }
+        }
+
+        if y+1 < 8 
+        {
+            if x+1 < 8
+            {
+                rangeOfMotion[x+3][y+1] = 1;
+            }
+            if x-1 >= 0
+            {
+                rangeOfMotion[x-3][y+1] = 1;
+            }
+        }
+        if y-1 >= 0
+        {
+            if x+3 < 8
+            {
+                rangeOfMotion[x+3][y-1] = 1;
+            }
+            if x-3 >= 0
+            {
+                rangeOfMotion[x-3][y-1] = 1;
+            }
+        }
+        
     }
 }
