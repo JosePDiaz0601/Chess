b42d96ed286fd0637896c6b706a826a7919dbf3e
Jose Diaz <josepd@uci.edu>
2022-04-12 12:43:45 -0700

    updating header file

--- chessPieces.h
+++ chessPieces.h
@@ -2,16 +2,16 @@
 #define PIECESHEADER
 
 enum color{
-black = 0;
-white = 1;}
+black = 0,
+white = 1};
 
 enum PIECETYPE{
-pawn   = 0;
-knight = 1;
-bishop = 2;
-rook   = 3;
-queen  = 4;
-king   = 5; }
+pawn   = 1,
+knight = 2,
+bishop = 3,
+rook   = 4,
+queen  = 5,
+king   = 6 };
 
 struct PIECE{
 int PIECETYPE,rank,file,color;
