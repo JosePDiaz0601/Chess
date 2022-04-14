773d43edbdcca9820fd6fe1c5f4aa72194a3db7c
Paul Khayet <pkhayet@uci.edu>
2022-04-14 11:19:48 -0700

    Making new board

--- chess.c
+++ chess.c
@@ -5,26 +5,29 @@
  *      Author: arhan
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include "chessPieces.h"
+#include "chess.h"
+
 /**#define ANSI_COLOR_RED  "\x1B[31m"
 #define ANSI_COLOR_GREEN  "\x1B[32m"
 #define ANSI_COLOR_BLUE  "\x1B[34m"
 #define Color_reset  "\x1B[0m"*/
 
-//array with initial positions of chess pieces, have the zeroth row correspond to white's first rank
-//columns go from a to h on board, 0 to 7 for array indices
+// array with initial positions of chess pieces, have the zeroth row correspond to white's first rank
+// columns go from a to h on board, 0 to 7 for array indices
 char positionValues[8][8][3] = {
-    {{'w','R','\0'},{'w','N','\0'},{'w','B','\0'},{'w','Q','\0'},{'w','K','\0'},{'w','B','\0'},{'w','N','\0'},{'w','R','\0'}}, //First rank of board , index 0 for row of array
-    {{'w','P','\0'},{'w','P','\0'},{'w','P','\0'},{'w','P','\0'},{'w','P','\0'},{'w','P','\0'},{'w','P','\0'},{'w','P','\0'}},	//second rank
-    {{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'}},	//third rank
-    {{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'}},	//fourth rank
-    {{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'}},	//fifth rank
-    {{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'},{' ',' ','\0'}},	//sixth rank
-    {{'b','P','\0'},{'b','P','\0'},{'b','P','\0'},{'b','P','\0'},{'b','P','\0'},{'b','P','\0'},{'b','P','\0'},{'b','P','\0'}},	//seventh rank of board, index 6 for row of array
-    {{'b','R','\0'},{'b','N','\0'},{'b','B','\0'},{'b','K','\0'},{'b','Q','\0'},{'b','B','\0'},{'b','N','\0'},{'b','R','\0'}} //eight rank, index 7 for row of array
+    {{'w', 'R', '\0'}, {'w', 'N', '\0'}, {'w', 'B', '\0'}, {'w', 'Q', '\0'}, {'w', 'K', '\0'}, {'w', 'B', '\0'}, {'w', 'N', '\0'}, {'w', 'R', '\0'}}, // First rank of board , index 0 for row of array
+    {{'w', 'P', '\0'}, {'w', 'P', '\0'}, {'w', 'P', '\0'}, {'w', 'P', '\0'}, {'w', 'P', '\0'}, {'w', 'P', '\0'}, {'w', 'P', '\0'}, {'w', 'P', '\0'}}, // second rank
+    {{' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}}, // third rank
+    {{' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}}, // fourth rank
+    {{' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}}, // fifth rank
+    {{' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}, {' ', ' ', '\0'}}, // sixth rank
+    {{'b', 'P', '\0'}, {'b', 'P', '\0'}, {'b', 'P', '\0'}, {'b', 'P', '\0'}, {'b', 'P', '\0'}, {'b', 'P', '\0'}, {'b', 'P', '\0'}, {'b', 'P', '\0'}}, // seventh rank of board, index 6 for row of array
+    {{'b', 'R', '\0'}, {'b', 'N', '\0'}, {'b', 'B', '\0'}, {'b', 'K', '\0'}, {'b', 'Q', '\0'}, {'b', 'B', '\0'}, {'b', 'N', '\0'}, {'b', 'R', '\0'}}  // eight rank, index 7 for row of array
 };
 // (0,0) corresponds to a1, (4,7) corresponds to d7, (7,7) corresponds to h8
-
 
 /**
 char player1Sign = 'X';
@@ -42,35 +45,40 @@ char playerUserMove[2] = {' ',' '};*/
 // main function that determines which version of game to play and whether to exit
 int main(void)
 {
-    int promptInput = 0;     //user's input assigned to promptInput
-    bool done = false;      //checks whether game is exited
+    int promptInput = 0; // user's input assigned to promptInput
+    bool done = false;   // checks whether game is exited
 
-    while (!done){
+    while (!done)
+    {
 
-        //display menu prompt
-        printf("Welcome to Unoptimized Chess, our newly released chess game from Loading…™!\n");
-//        printf("2. Start New Game (Human v. Human)\n");
+        // display menu prompt
+        printf("Welcome to Unoptimized Chess, our newly released chess game from Loadingï¿½ï¿½!\n");
+        //        printf("2. Start New Game (Human v. Human)\n");
         printf("1. Start New Game (Human v. AI)\n");
         printf("2. Game Settings\n");
         printf("3. Exit Game\n");
         printf("Choose Option: ");
         scanf("%d", &promptInput);
         int j;
-        if (promptInput == 1){
-            //print board with starting position
-        	printf("  +----+----+----+----+----+----+----+----+\n");
-        	for(int i = 8; i > 0; i--){
-        		j= 0;
-        		printf("%d | %s | %s | %s | %s | %s | %s | %s | %s |\n", i, positionValues[i-1][j], positionValues[i-1][j+1] , positionValues[i-1][j+2], positionValues[i-1][j+3], positionValues[i-1][j+4] , positionValues[i-1][j+5] , positionValues[i-1][j+6] ,positionValues[i-1][j+7] );
-				printf("  +----+----+----+----+----+----+----+----+\n");
-        	}
-        	printf("    a    b    c    d    e    f    g    h\n");
-//        	playAgainstAI();
+        if (promptInput == 1)
+        {
+            // print board with starting position
+            printf("  +----+----+----+----+----+----+----+----+\n");
+            for (int i = 8; i > 0; i--)
+            {
+                j = 0;
+                printf("%d | %s | %s | %s | %s | %s | %s | %s | %s |\n", i, positionValues[i - 1][j], positionValues[i - 1][j + 1], positionValues[i - 1][j + 2], positionValues[i - 1][j + 3], positionValues[i - 1][j + 4], positionValues[i - 1][j + 5], positionValues[i - 1][j + 6], positionValues[i - 1][j + 7]);
+                printf("  +----+----+----+----+----+----+----+----+\n");
+            }
+            printf("    a    b    c    d    e    f    g    h\n");
+            //        	playAgainstAI();
         }
-        else if (promptInput == 2){
-//            viewGameSettings();
+        else if (promptInput == 2)
+        {
+            //            viewGameSettings();
         }
-        else if (promptInput == 3){
+        else if (promptInput == 3)
+        {
             done = true;
         }
         printf("\n\n\n\n");
@@ -78,11 +86,23 @@ int main(void)
     return 0;
 }
 
-void viewGameSettings(){
-
+void viewGameSettings()
+{
 }
-//update these functions in the future
-void playAgainstAI(){
-
+// update these functions in the future
+void playAgainstAI()
+{
 }
+
+void newBoard()
+{
+    struct PIECE board[8][8];
+    PIECE *pawn1;
+    pawn1 = (PIECE *)malloc(sizeof(IMAGE));
+    // Instantiate pawns
+    for (int i = 0; i < 8; i++)
+    {
+    }
+}
+
 /* EOF */
--- chessPieces.h
+++ chessPieces.h
@@ -1,22 +1,31 @@
 #ifndef PIECESHEADER
 #define PIECESHEADER
 
-enum color{
-black = 0,
-white = 1};
+#include <stdbool.h>
 
-enum PIECETYPE{
-pawn   = 1,
-knight = 2,
-bishop = 3,
-rook   = 4,
-queen  = 5,
-king   = 6 };
+enum color
+{
+    black = 0,
+    white = 1
+};
 
-struct PIECE{
-int PIECETYPE,rank,file,color;
-bool hasMoved;
-}
+enum PIECETYPE
+{
+    pawn = 1,
+    knight = 2,
+    bishop = 3,
+    rook = 4,
+    queen = 5,
+    king = 6
+};
 
+struct PIECE
+{
+    int type;
+    int rank; // Numbers
+    int file; // Letters
+    char color;
+    bool hasMoved;
+};
 
 #endif
