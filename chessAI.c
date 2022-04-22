e7e0670f2df2b2170d5d714275b20fd2680b6a38
Paul Khayet <pkhayet@uci.edu>
2022-04-22 16:40:20 -0700

    Created a protype random move AI

--- chessAI.c
+++ chessAI.c
@@ -1,15 +1,49 @@
 /*********************************************************************/
 /* Chess Assignment, for EECS 22L, Spring 22                         */
 /*                                                                   */
-/* Initial author: Paul Khayet                                       */
+/* Initial author: Batman (Paul) Robin (Cameron)                     */
 /*                                                                   */
 /* ChessAI.c: C file for Artifical Intelligence                      */
 /*                                                                   */
 /*********************************************************************/
 
 #include <stdlib.h>
+#include <time.h>
 #include "chessAI.h"
+#include "chessPieces.h"
 
-void choosePiece(); 
-
-
+void randomMove()
+{
+    // First is location on board, second is the available locations to move on the board
+    int moves[64][64];
+    int temp[64];
+    int succ = 0;
+    time_t t;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            temp[64] = getRangeOfMotion(board[0][i][j].type, board[0][i][j].color, i, j, board[0][i][j].hasMoved);
+            for (int k = 0; k < 64; k++)
+            {
+                moves[i + j][k] = temp[k];
+            }
+        }
+    }
+    // Inefficent asf because it does 2 rands a misses a lot before making a move
+    while (succ == 0)
+    {
+        srand((unsigned)time(&t));
+        int location = (rand() % 64);
+        int moveset = (rand() % 64);
+        if (moves[location][moveset] == 0)
+        {
+            continue;
+        }
+        else if (moves[location][moveset] == 1)
+        {
+            movePiece((location / 8), (location % 8), (moveset / 8), (moveset % 8));
+            succ = 1;
+        }
+    }
+}
