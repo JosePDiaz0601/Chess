4b669b3e5bf00a64de1912e42d42617139978df4
Jose Diaz <josepd@uci.edu>
2022-04-23 16:42:55 -0700

    LogFile improved/finished?

--- LogFile.h
+++ LogFile.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 
-int WriteToLog(char MovePart1[2],char MovePart2[2],char MovePiece[2]);          //Write to log takes whatever is in 'move' array and Writes it to the LogFile
+int WriteToLog(int movenumber,char MovePart1[],char MovePart2[],char MovePiece[]);          //Write to log takes whatever is in 'move' array and Writes it to the LogFile
 
 void DeleteMove(void);                   //Should delete the indicated move. Useful for situations where we allow user to redo their move.
 
