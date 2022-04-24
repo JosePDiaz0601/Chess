50576e7aa3767fafd6466969e187aef375ce93f8
Arhant Katare <akatare@uci.edu>
2022-04-24 14:23:20 -0700

    Making user's and computer's move array available to Logfile.c

--- chess.c
+++ chess.c
@@ -10,6 +10,9 @@
 #include "chessPieces.c"
 #include "chessAI.c"
 #include "chessAI.h"
+#include "LogFile.c"
+#include "Logfile.h"
+#include "chess.h"
 
 int userColor; //the int that user types to choose color 0 for white 1 for black
 // array with initial positions of chess pieces, have the zeroth row correspond to white's first rank
@@ -147,9 +150,9 @@ int main(void)
     turn = 0;
     //when turn % 2 == 0 (white's Turn), when turn % 2 == 1 (black's Turn)
 
-    char movePart1[3], movePart2[3];
+/**    char movePart1[3], movePart2[3];
     char computerMovePart1[3];
-    char computerMovePart2[3];
+    char computerMovePart2[3];*/
     while (!programFinished)
     {
         displayStartingMenu();
