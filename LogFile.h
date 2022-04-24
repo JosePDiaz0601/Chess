851cd869a252f727565b0a4c90ff1cffe04c16a2
Jose Diaz <josepd@uci.edu>
2022-04-24 14:27:13 -0700

    Updating LogFile

--- LogFile.h
+++ LogFile.h
@@ -2,9 +2,10 @@
 #define LOGFILEHEADER
 
 #include <stdio.h>
+#include <string.h>
 
 
-int WriteToLog(int movenumber,char MovePart1[],char MovePart2[],char MovePiece[]);          //Write to log takes whatever is in 'move' array and Writes it to the LogFile
+int WriteToLog(char MovePart1[],char MovePart2[],char JustMovedPieceColor, char JustMovedPieceType);          //Write to log takes whatever is in 'move' array and Writes it to the LogFile
 
 void DeleteMove(void);                   //Should delete the indicated move. Useful for situations where we allow user to redo their move.
 
