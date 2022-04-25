b6225fe7f7e26166820267aeec9abb9be7077c89
Maher Homsi <homsim@uci.edu>
2022-04-24 23:08:14 -0700

    fix ai

--- chessPieces.c
+++ chessPieces.c
@@ -619,7 +619,7 @@ int check(char king, int boardNumber)
 			if ((board[boardNumber][y][x].type == 4 || board[boardNumber][y][x].type == 5))
 			{
 				isInCheck = 24;
-			}else if (board[boardNumber][y][x].type == 1 || board[boardNumber][y][x].type == 2 || board[boardNumber][y][x].type == 3 board[boardNumber][y][x].type == 6){
+			}else if (board[boardNumber][y][x].type == 1 || board[boardNumber][y][x].type == 2 || board[boardNumber][y][x].type == 3 || board[boardNumber][y][x].type == 6){
 				break;
 			}
 			break;
@@ -635,7 +635,7 @@ int check(char king, int boardNumber)
 			if ((board[boardNumber][y][x].type == 4 || board[boardNumber][y][x].type == 5))
 			{
 				isInCheck = 23;
-			}else if (board[boardNumber][y][x].type == 1 || board[boardNumber][y][x].type == 2 || board[boardNumber][y][x].type == 3 board[boardNumber][y][x].type == 6){
+			}else if (board[boardNumber][y][x].type == 1 || board[boardNumber][y][x].type == 2 || board[boardNumber][y][x].type == 3 || board[boardNumber][y][x].type == 6){
 				break;
 			}
 			break;
@@ -651,7 +651,7 @@ int check(char king, int boardNumber)
 			if ((board[boardNumber][y][x].type == 4 || board[boardNumber][y][x].type == 5))
 			{
 				isInCheck = 22;
-			}else if (board[boardNumber][y][x].type == 1 || board[boardNumber][y][x].type == 2 || board[boardNumber][y][x].type == 3 board[boardNumber][y][x].type == 6){
+			}else if (board[boardNumber][y][x].type == 1 || board[boardNumber][y][x].type == 2 || board[boardNumber][y][x].type == 3 || board[boardNumber][y][x].type == 6){
 				break;
 			}
 			break;
@@ -669,7 +669,7 @@ int check(char king, int boardNumber)
 			if ((board[boardNumber][y][x].type == 4 || board[boardNumber][y][x].type == 5))
 			{
 				isInCheck = 21;
-			}else if (board[boardNumber][y][x].type == 1 || board[boardNumber][y][x].type == 2 || board[boardNumber][y][x].type == 3 board[boardNumber][y][x].type == 6){
+			}else if (board[boardNumber][y][x].type == 1 || board[boardNumber][y][x].type == 2 || board[boardNumber][y][x].type == 3 || board[boardNumber][y][x].type == 6){
 				break;
 			}
 			break;
@@ -688,7 +688,7 @@ int check(char king, int boardNumber)
 			if ((board[boardNumber][y][x].type == 3 || board[boardNumber][y][x].type == 5))
 			{
 				isInCheck = 20;
-			}else if (board[boardNumber][y][x].type == 1 || board[boardNumber][y][x].type == 2 || board[boardNumber][y][x].type == 4 board[boardNumber][y][x].type == 6){
+			}else if (board[boardNumber][y][x].type == 1 || board[boardNumber][y][x].type == 2 || board[boardNumber][y][x].type == 4 || board[boardNumber][y][x].type == 6){
 				break;
 			}
 			break;
@@ -707,7 +707,7 @@ int check(char king, int boardNumber)
 			if ((board[boardNumber][y][x].type == 3 || board[boardNumber][y][x].type == 5))
 			{
 				isInCheck = 19;
-			}else if (board[boardNumber][y][x].type == 1 || board[boardNumber][y][x].type == 2 || board[boardNumber][y][x].type == 4 board[boardNumber][y][x].type == 6){
+			}else if (board[boardNumber][y][x].type == 1 || board[boardNumber][y][x].type == 2 || board[boardNumber][y][x].type == 4 || board[boardNumber][y][x].type == 6){
 				break;
 			}
 			break;
@@ -727,7 +727,7 @@ int check(char king, int boardNumber)
 			{
 				// printf("%d %d %c %d", x, y, board[boardNumber][y][x].color, board[boardNumber][y][x].type);
 				isInCheck = 18;
-			}else if (board[boardNumber][y][x].type == 1 || board[boardNumber][y][x].type == 2 || board[boardNumber][y][x].type == 4 board[boardNumber][y][x].type == 6){
+			}else if (board[boardNumber][y][x].type == 1 || board[boardNumber][y][x].type == 2 || board[boardNumber][y][x].type == 4 || board[boardNumber][y][x].type == 6){
 				break;
 			}
 			break;
@@ -746,7 +746,7 @@ int check(char king, int boardNumber)
 			if ((board[boardNumber][y][x].type == 3 || board[boardNumber][y][x].type == 5))
 			{
 				isInCheck = 17;
-			}else if (board[boardNumber][y][x].type == 1 || board[boardNumber][y][x].type == 2 || board[boardNumber][y][x].type == 4 board[boardNumber][y][x].type == 6){
+			}else if (board[boardNumber][y][x].type == 1 || board[boardNumber][y][x].type == 2 || board[boardNumber][y][x].type == 4 || board[boardNumber][y][x].type == 6){
 				break;
 			}
 			// printf("EXAMINE queen CHECK");
